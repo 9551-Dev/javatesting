1c6c708332be7db65dd0ced15349c52c479cfa44
9551Dev <oliverekcaha@gmail.com>
2022-08-29 18:05:34 +0200

    Initial commit

---
+++ src/nffodev/idk/net/Question.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import AnwserCorrect;
+import AnwserWrong;
+
+public class Question {
+    String question;
+
+    public Question(String question, ArrayList<AnwserCorrect>, ArrayList<AnwserWrong>) {
+
+    }
+}
